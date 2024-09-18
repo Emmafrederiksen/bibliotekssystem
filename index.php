2dcc332e55055991b1397cfd89b08f30ca4bbe89
emmao <emf002@edu.zealand.dk>
2024-09-18 14:29:32 +0200

    Man kan nu se beskrivelse som er forkortet med text-truncate så det ikke fylder det hele på denne side.

--- index.php
+++ index.php
@@ -27,8 +27,8 @@ require "settings/init.php";
         $biblioteker = $db->sql("SELECT * FROM books ORDER BY bookId");
         foreach($biblioteker as $bibliotek) {
             ?>
-            <div class="col-12 col-md-4">
-                <div class="card w-100">
+            <div class="col-12 col-md-4 ">
+                <div class="card w-100 h-100 d-flex flex-column">
                     <div class="card-header bg-info-subtle text-black py-3">
                         <?php
                         echo "<h3 class='m-0'>".$bibliotek->bookTitle . "<span class='text-secondary'> / " . $bibliotek->bookYear . "</span></h3>";
@@ -37,16 +37,18 @@ require "settings/init.php";
                     <div class="card-body">
                         <?php
                         if (!empty($bibliotek->bookImage)) {
-                            echo "<img src='img/" . $bibliotek->bookImage . "' alt='" . $bibliotek->bookTitle . "' class='d-block mx-auto' style='max-width: 100%; height: 400px;'>";
+                            echo "<img src='img/" . $bibliotek->bookImage . "' alt='" . $bibliotek->bookTitle . "' class='d-block mx-auto' style='max-width: 100%; height: 400px; object-fit: cover; object-position: center;'>";
                         } else {
                             echo "Intet billede tilgængeligt";
                         }
                         ?>
                     </div>
 
-                    <div class="card-body">
-                        Genre: <?php echo $bibliotek->bookGenre; ?>
+                    <div class="card-body mt-auto">
+                        <p><strong>Genre:</strong> <?php echo $bibliotek->bookGenre; ?></p>
+                        <p class="text-truncate"><strong>Beskrivelse:</strong> <?php echo $bibliotek->bookDescription; ?></p>
                     </div>
+
                     <div class="card-footer text-muted">
                         <a class="btn btn-outline-secondary stretched-link" href="book.php?bookId=<?php echo $bibliotek->bookId; ?>" role="button">Læs mere</a>
                     </div>
